6a763731ccaff403edc87df95f01551a44f2c1d2
Lucas Alexsandro <devlucasalexsandro@gmail.com>
2022-01-20 19:58:29 -0300

    Confirmar

--- modulo-01/exercicios/Aula03/ex05.js
+++ modulo-01/exercicios/Aula03/ex05.js
@@ -1,1 +1,11 @@
 const prompt = require("prompt-sync")();
+
+let A = +prompt("Digite o valor paara o lado 1: ");
+let B = +prompt("Digite o valor paara o lado 2: ");
+let C = +prompt("Digite o valor paara o lado 3: ");
+
+console.log();
+console.log("----- LADOS -----");
+console.log(`A: ${A} | B: ${B} | C: ${C}`);
+console.log("------------------");
+console.log();
