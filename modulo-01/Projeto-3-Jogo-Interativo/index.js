d48e328bcce9eb5dfa05a225dba7cb216f193c2f
lucasalexsandro <devlucasalexsandro@gmail.com>
2022-02-15 07:22:44 -0300

    Atualização

--- modulo-01/Projeto-3-Jogo-Interativo/index.js
+++ modulo-01/Projeto-3-Jogo-Interativo/index.js
@@ -44,28 +44,69 @@ console.log();
 let nomePersonagem = prompt('Qual o nome do seu Personagem? ');
 let idadePersonagem = +prompt('Qual a idade do seu Personagem? ');
 
-// IDEIA PARA DEFINIÇÃO DE INIMIGOS DO PROJETO
+// DEFININDO INIMIGOS
 let monstros = {
+
+    //Monstro  de dano Baixo
     monstroFacil: {
-        nome: 'Monster',
+        nome: 'Grifo',
         level: 1,
         dano: 10,
-        frase: "",
-        vida: 100
+        frase: ["HAHAHA VOCÊ É FRACO!", "NÃO CONSEGUE ME VENCER", "TOMA ESSA"],
+        vida: 100,
+
+        //Funções Monstros
+        aumentaVida: function (valor) {
+            if (this.vida >= 100) {
+                console.log("Vida no máximo!");
+            } else {
+                this.vida = this.vida + valor;
+            }
+        },
+        retiraVida: function (valor) {
+            this.vida = this.vida - valor;
+        }
     },
+
+    // Monstro de Dano Médio
     monstroMedio: {
-        nome: 'Monster 2',
-        level: 1,
+        nome: 'Estrige',
+        level: 3,
         dano: 20,
-        frase: "",
-        vida: 100
+        frase: ["HAHAHA VOCÊ É FRACO!", "NÃO CONSEGUE ME VENCER", "TOMA ESSA"],
+        vida: 100,
+
+        //Funções Monstros
+        aumentaVida: function (valor) {
+            if (this.vida >= 100) {
+                console.log("Vida no máximo!");
+            } else {
+                this.vida = this.vida + valor;
+            }
+        },
+        retiraVida: function (valor) {
+            this.vida = this.vida - valor;
+        }
     },
+    // Monstro de Maior Dano
     monstroDificil: {
         nome: 'Monster 3',
-        level: 1,
+        level: 5,
         dano: 30,
-        frase: "",
-        vida: 100
+        frase: ["HAHAHA VOCÊ É FRACO!", "NÃO CONSEGUE ME VENCER", "TOMA ESSA"],
+        vida: 100,
+
+        //Funções Monstros
+        aumentaVida: function (valor) {
+            if (this.vida >= 100) {
+                console.log("Vida no máximo!");
+            } else {
+                this.vida = this.vida + valor;
+            }
+        },
+        retiraVida: function (valor) {
+            this.vida = this.vida - valor;
+        }
     }
 }
 
@@ -74,12 +115,13 @@ let monstros = {
 let personagem = {
     nome: nomePersonagem,
     idade: idadePersonagem,
-    dano: 20,
+    dano: 30,
     level: 0,
     cash: 0,
     fome: 0,
+    espada: "Luz das Cinzas",
     missoesFinalizadas: 0,
-    reinoInicial: "",
+    reinoInicial: "TEMERIA",
     xp: 0,
     vida: 100,
     status: function () {
@@ -94,59 +136,63 @@ let personagem = {
     },
     // Retira a vida do personagem
     retiraVida: function (valor) {
-        this.vida = this.vida - valor;
-
-        if(this.vida < 1){
+        if (this.vida < 1) {
             console.log("Você Morreu!");
+        } else {
+            this.vida = this.vida - valor;
         }
     },
     // Aumenta a Vida
     aumentaVida: function (valor) {
-        this.vida = this.vida + valor;
-
-        if(this.vida > 100){
+        if (this.vida > 100) {
             console.log("Sua vida está totalmente cheia!");
+        } else {
+            this.vida = this.vida + valor;
         }
     },
     // Escolhe o reino
     escolhaReino: function (valor) {
         this.reinoInicial = valor;
     },
-    // Define a dificuldade
-    dificuldade: function (reinoInicial) {
-        if (this.dificuldade == 1) {
-            monstros.monstroFacil;
-        } else if (this.dificuldade == 2) {
-            monstros.monstroMedio;
-        } else if (this.dificuldade == 3) {
-            monstros.monstroDificil;
-        }
-    },
     //Aumenta a Fome
-    aumentaFome: function(){
+    aumentaFome: function () {
         this.fome = 20;
 
-        if(this.fome > 100){
+        if (this.fome > 100) {
             console.log("Você morreu, Fim de Jogo!!!!")
         }
     },
     //Aumenta o Dano do Personagem
-    aumentaDano: function(valor){
+    aumentaDano: function (valor) {
         this.dano = this.dano = valor;
 
-        if(this.dano >= 100){
+        if (this.dano >= 100) {
             console.log("Seu dano está no máximo!")
         }
     },
     // Diminui a FOME
-    diminuiFome: function(valor){
+    diminuiFome: function (valor) {
         this.fome - valor
 
-        if(this.fome < 0 ){
+        if (this.fome < 0) {
             console.log("Você não está com fome!");
         }
     }
 }
+
+// Definindo Mercado
+
+let mercado = [
+    {espada: 'Em chamas', dano: function(){
+        personagem.dano = +20;
+    }},
+    {espada: 'Endemoniada', dano: function(){
+        personagem.dano = +25;
+    }},
+    {espada: 'Massacre', dano: function(){
+        personagem.dano = +30;
+    }}  
+]
 
 // DEFININDO O TEMPO INICIAL DO JOGO
 let tempoJogo = {
@@ -163,7 +209,7 @@ let tempoJogo = {
         }
     },
     informaTempo: function () {
-        console.log("O horário do jogo é: " + tempoJogo.horario);
+        console.log("O horário do jogo é: " + tempoJogo.horario + ' Hrs');
         console.log("O dia do jogo é: " + tempoJogo.dia);
     }
 
@@ -171,90 +217,114 @@ let tempoJogo = {
 
 // FUNCTIONS
 
-function batalha(){
-
-}
-
-// INICIANDO HISTÓRIA DO GAME
-
-let confirmaAcao = 'n';
-
-do {
-    console.log(`
-    Olá ${personagem.nome} bem vindo ao game RPG
-    --------------------------------------------
-    O objetivo do game é você realizar missões para 
-    os habitantes em algum reino de sua escolha, 
-    lembre-se que dependendo do reino o seu ganho
-    ao concluir uma missão pode ser maior, mas com
-    certeza os desafios também serão!
-`)
-
-    confirmaAcao = prompt("Digite [SIM] para confirmar a mensagem: ")
-
-} while (confirmaAcao != 'sim' && confirmaAcao != 'SIM' && confirmaAcao != "s");
-
-console.clear();
-
-// DEFININDO REINO DO JOGO
-
-console.log(`#################### ESCOLHA O REINO ####################`);
-console.log(`[1] - REINO 1`);
-console.log(`[2] - REINO 2`);
-console.log(`[3] - REINO 3`);
-console.log(`################################################`);
-let escolhaReino = +prompt('---> ');
-personagem.escolhaReino(escolhaReino);
-
-let reinoEscolhido = personagem.reinoInicial;
-console.log("O Reino inicial é: " + personagem.reinoInicial);
-
-// DEFININDO MISSÃO INICIAL DO REINO ESCOLHIDO
-
-confirmaAcao = 'n';
-
-do {
-    console.log(`
-    Olá ${personagem.nome} Bem vindo ao Reino de ${personagem.reinoInicial}
-    --------------------------------------------
-    Você recebeu uma missão inicial do reino para
-    mostrar o seu valor! Mate o Monstro...
-    ---------------------------------------------
-    O horário agora é: ${tempoJogo.horario}
-    O dia agora é: ${tempoJogo.dia}
-`)
-
-    confirmaAcao = prompt("Digite [SIM] para confirmar a mensagem: ")
-
-} while (confirmaAcao != 'sim' && confirmaAcao != 'SIM' && confirmaAcao != "s");
-
-
 // DEFININDO MISSÕES
 
-let missoes = {
+function missao1() {
+    console.log("************** MISSÃO INICIAL **************")
+    console.log();
+    console.log(`Você recebeu a missão de matar um monstro que está atormentando a vila no reino de ${personagem.reino}`);
+    console.log();
+    console.log(`O monstro ${monstros.monstroFacil.nome} está rodeando você!`);
+    console.log(`Ele faz um primeiro ataque, mas você acaba esquivando...`);
+    console.log("Deseja atacar? [SIM] - [NÃO]")
+    let ataque = prompt("---> ");
+
+    if(ataque == 'sim'){
+        for(let i; monstros.monstroFacil.vida < 0; personagem.dano++){
+        console.log(`A vida do monstro é: ${monstros.monstroFacil.vida}`);
+    }
+    }
 
 }
 
+function missao2(valor) {
+
+}
+
+function missao3(valor) {
+
+}
+
+function missao4(valor) {
+
+}
+
+function missao5(valor) {
+
+}
+
+// INICIANDO O GAME
 
 
+inicioJogo = true;
+while (inicioJogo == true) {
+    // INICIANDO TESTOS INICIAIS DO JOGO
+
+    let confirmaAcao = 'n';
+
+    do {
+        console.log(`
+          Olá ${personagem.nome} bem vindo ao game RPG
+          --------------------------------------------
+          O objetivo do game é você realizar missões para 
+          os habitantes em algum reino de sua escolha, 
+          lembre-se que dependendo do reino o seu ganho
+          ao concluir uma missão pode ser maior, mas com
+          certeza os desafios também serão!
+      `)
+
+        confirmaAcao = prompt("Digite [SIM] para confirmar a mensagem: ")
+
+    } while (confirmaAcao != 'sim' && confirmaAcao != 'SIM' && confirmaAcao != "s");
+
+    console.clear();
+    console.log();
+
+    // DEFININDO MISSÃO INICIAL DO REINO ESCOLHIDO
+
+    confirmaAcao = 'n';
+
+    do {
+        console.log(`
+          Olá ${personagem.nome} Bem vindo ao Reino de ${personagem.reinoInicial}
+          --------------------------------------------
+          Você recebeu uma missão inicial do reino para
+          mostrar o seu valor! 
+          ---------------------------------------------
+      `)
+        tempoJogo.informaTempo();
+
+        confirmaAcao = prompt("Digite [SIM] para confirmar a mensagem: ")
+
+    } while (confirmaAcao != 'sim' && confirmaAcao != 'SIM' && confirmaAcao != "s");
+
+    console.log();
+    console.clear();
+
+    missao1();
+    tempoJogo.alteraTempo(10);
+
+    // JORNADA ATÉ OUTRA CIDADE
+    console.log("Você está a caminho da cidade mais proxima para encontrar novas missões....")
+    tempoJogo.alteraTempo(20);
+
+    // SEGUNDA MISSÃO - CASO O JOGADOR DECIDA ACEITAR
+
+    // TERCEIRA MISSÃO QUE VEM AO JOGADOR ACEITAR A SEGUNDA MISSÃO - SÓ APARECE CASO ELE ACEITE A SEGUNDA
+
+    // QUARTA MISSÃO DEPOIS DE UMA LONGA JORNADA
+
+    console.log("Você encontrou um mercador no caminho, deseja comprar uma espada mais forte?");
+    let mercador = prompt("sim ou nao");
+
+    if(mercador == 'sim'){
+        for(let i of mercado){
+            console.log(i);
+        }
+    }
 
 
-
-
-
-
-
-
-
-
-
-
-
-// do {
-//     console.log(`
-//     ${personagem.nome} Você chegou a um mercador
-//     --------------------------------------------
-
-// `)
-//     confirmaAcao = prompt("Digite [SIM] para confirmar a mensagem: ")
-// }while(confirmaAcao != 'sim' && confirmaAcao != 'SIM' && confirmaAcao != "s");
+    // PERGUNTANDO SE O JOGADOR DESEJA JOGAR NOVAMENTE!;
+   
+    
+}
